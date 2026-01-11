845f61eb556f279dacbb45baec599b916176fdfd
Tim Rabinus <mbuis.public@gmail.com>
2026-01-11 19:11:05 +1300

    Initial Commit

---
+++ Context/ContextApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContextApp.swift
+//  Context
+//
+//  Created by Martin on 11/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct ContextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
